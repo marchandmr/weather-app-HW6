9964a6e201a8edb7a5ca8eb92c599bdd20d4a5d6
Matthew Marchand <marchandmr@icloud.com>
2020-11-19 16:01:40 -0600

    updated js, and added classes to change the color of the UV index

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 var submitBtn = $(".submit-btn");
 var key = "6cf37dfb9a0f723626baf273dac3a69b";
-
-
-submitBtn.click(function () {
+var dateIDcounter = ["0", "1", "2", "3", "4"];
+var array = [];
+submitBtn.click(function (event) {
+    event.preventDefault();
+    $("#city").empty();
+    $("#picture").empty();
+    $("#temperature").empty();
+    $("#humidity").empty();
+    $("#wind").empty();
+    $("#wind").empty();
+    $("#uvIndex").empty();
     var textBox = $(".input").val();
+    array.push(textBox);
+    console.log(array);
+    localStorage.setItem("textBox", JSON.stringify(textBox));
+    var get = JSON.parse(localStorage.getItem("textBox"));
+    $(".list-group").append("<li>" + get + "</li>");
+
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + textBox + "&Appid=" + key + "&units=imperial";
     var fiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + textBox + "&Appid=" + key + "&units=imperial";
 
@@ -17,9 +31,9 @@ submitBtn.click(function () {
             var lat = response.coord.lat;
             var lon = response.coord.lon;
             var uvIndex = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + key;
-            $("#city").append("<h4>" + response.name + "</h4>");
+            $("#city").append("<h4>" + response.name + " " + "(" + (moment().format('MM-DD-YYYY')) + ")" + "</h4>");
             $("#picture").append("<img src=" + pictureURL + ">");
-            $("#temperature").append("<p>" + "temperature: " + Math.floor(response.main.temp) + "</p>");
+            $("#temperature").append("<p>" + "temperature: " + Math.floor(response.main.temp) + " F" + "</p>");
             $("#humidity").append("<p>" + "humidity: " + response.main.humidity + "%" + "</p>");
             $("#wind").append("<p>" + "wind speed: " + response.wind.speed + " mph" + "</p>");
 
@@ -28,10 +42,47 @@ submitBtn.click(function () {
                 method: "GET"
             })
                 .then(function (response) {
+                    $("#uvIndex").removeClass("favorable");
+                    $("#uvIndex").removeClass("moderate");
+                    $("#uvIndex").removeClass("severe");
                     $("#uvIndex").append("<p>" + "UV Index:  " + response.value + "</p>");
+                    var uv = response.value;
+                    if (uv > 0 && uv <= 2) {
+                        $("#uvIndex").addClass("favorable")
+                    }
+                    else if (uv > 3 && uv <= 5.9) {
+                        $("#uvIndex").addClass("moderate")
+                    }
+                    else {
+                        $("#uvIndex").addClass("severe")
+                    };
 
                 })
         })
 
+    $.ajax({
+        url: fiveDay,
+        method: "GET"
+    })
+        .then(function (response) {
+
+            console.log(fiveDay)
+            for (var counter = 0; counter < dateIDcounter.length; counter++) {
+                $("#date" + dateIDcounter[counter]).empty();
+                $("#img" + dateIDcounter[counter]).empty();
+                $("#temp" + dateIDcounter[counter]).empty();
+                $("#hum" + dateIDcounter[counter]).empty();
+                $("#date" + dateIDcounter[counter]).append(response.list[((counter + 1) * 8) - 1].dt_txt);
+                var picture = response.list[((counter + 1) * 8) - 1].weather[0].icon;
+                var pictureURL = "https://openweathermap.org/img/wn/" + picture + ".png";
+                $("#img" + dateIDcounter[counter]).append("<img src=" + pictureURL + ">");
+                $("#temp" + dateIDcounter[counter]).append("<p>" + "Temp: " + Math.floor(response.list[((counter + 1) * 8) - 1].main.temp) + "F" + "</p>");
+                $("#hum" + dateIDcounter[counter]).append("<p>" + "Humidity: " + response.list[((counter + 1) * 8) - 1].main.humidity + "%" + "</p>");
+
+            }
+        })
+
+
 })
+
 
